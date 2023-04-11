6778155ddab210d8408481294622f6e8a031dca7
Emil Andersen <emiand@norlys.dk>
2023-04-11 13:20:16 +0200

    add login page

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -18,7 +18,7 @@ function Copyright(props: any) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Investment Service
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,9 @@ import LoginForm from "../components/forms/LoginForm";
 
 const LoginPage = () => {
     return (
-        <LoginForm />
+        <>
+            <LoginForm />
+        </>
     )
   }
   export default LoginPage
