4900cda7a1390a464f4e3fc10290ab5300baa287
Emil Andersen <emiand@norlys.dk>
2023-04-11 13:03:57 +0200

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import HomePage from "./pages/HomePage"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <Container maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="text.primary"
-          gutterBottom
-        >
-          Investment Service
-        </Typography>
-        <Typography variant="h5" align="center" color="text.secondary" component="p">
-          React typescript app styled with Material UI.
-        </Typography>
-      </Container>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<p>Not found</p>} />
+            <Route path="/login" element={<p>Login</p>} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
